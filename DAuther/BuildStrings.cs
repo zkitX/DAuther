a4db8a712e5462d68bce4fd04311356ac55ef560
simonmkwii-dev <40786398+simonmkwii-dev@users.noreply.github.com>
2018-07-21 04:25:05 +1000

    Forgot to push changes...

--- DAuther/BuildStrings.cs
+++ DAuther/BuildStrings.cs
@@ -11,13 +11,17 @@ namespace DAuther
     {
         public static string URLSafe(string Str)
         {
-            return Str.Replace("+", "-").Replace("/", "_").Replace("=","");
+            return Str.Replace("+", "-").Replace("/", "_").Replace("=", "");
         }
         public static byte[] MasterKey = { 0xCF, 0xA2, 0x17, 0x67, 0x90, 0xA5, 0x3F, 0xF7, 0x49, 0x74, 0xBF, 0xF2, 0xAF, 0x18, 0x09, 0x21 };
         public static byte[] AESUseSrc = { 0x4D, 0x87, 0x09, 0x86, 0xC4, 0x5D, 0x20, 0x72, 0x2F, 0xBA, 0x10, 0x53, 0xDA, 0x92, 0xE8, 0xA9 };
         public static byte[] DAuth_KEK = { 0x8B, 0xE4, 0x5A, 0xBC, 0xF9, 0x87, 0x02, 0x15, 0x23, 0xCA, 0x4F, 0x5E, 0x23, 0x00, 0xDB, 0xF0 };
         public static byte[] DAuth_Src = { 0xDE, 0xD2, 0x4C, 0x35, 0xA5, 0xD8, 0xC0, 0xD7, 0x6C, 0xB8, 0xD7, 0x8C, 0xA0, 0xA5, 0xA5, 0x22 };
-        public static string Client_ID_NIM = "93af0acb26258de9";
+        public static string[] Client_ID =
+        {
+        "93af0acb26258de9",
+        "81333c548b2e876d"
+        };
         public static string UserAgent = string.Format("libcurl (nnDauth; {0}; SDK {1}.{2}.{3}.{4}; Add-on {1}.{2}.{3}.{4})", "789f928b-138e-4b2f-afeb-1acae821d897", 5, 3, 0, 0);
         public static string Challenge = string.Format("key_generation={0}", 5);
         public static string SysDigest = "gW93A#00050100#29uVhARHOdeTZmfdPnP785egrfRbPUW5n3IAACuHoPw=";
@@ -26,15 +30,18 @@ namespace DAuther
         {
             return true;
         }
-        public static string MakeReq(string URL, byte[] PostData)
+        public static string MakeReq(string URL, byte[] PostData, int ContentLength)
         {
             try
             {
                 ServicePointManager.ServerCertificateValidationCallback = AcceptAllCertifications;
                 HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(URL);
                 Request.ClientCertificates.Add(Cert);
+                Request.Host = "dauth-lp1.ndas.srv.nintendo.net";
                 Request.UserAgent = UserAgent;
                 Request.Accept = "*/*";
+                Request.ContentLength = ContentLength;
+                Request.ContentType = "application/x-www-form-urlencoded";
                 Request.Method = "POST";
                 Stream DataStream = Request.GetRequestStream();
                 DataStream.Write(PostData, 0, PostData.Length);
@@ -66,9 +73,13 @@ namespace DAuther
             byte[] GenAESKek = Decrypt(DAuth_KEK, GenAESKey);
             return Decrypt(DAuth_Src, GenAESKek);
         }
-        public static string BuildRequestString(string Challenge)
+        public static string BuildFirstRequestString(string Challenge)
         {
-            return string.Format("challenge={0}&client_id={1}&key_generation={2}&system_version={3}", Challenge, Client_ID_NIM, 5, SysDigest);
+            return string.Format("challenge={0}&client_id={1}&key_generation={2}&system_version={3}", Challenge, Client_ID[0], 5, SysDigest);
+        }
+        public static string BuildSecondRequestString(string Challenge)
+        {
+            return string.Format("challenge={0}&client_id={1}&key_generation={2}&system_version={3}", Challenge, Client_ID[1], 5, SysDigest);
         }
         public static string GenerateCMACOfRequestString(byte[] Key, string RequestData)
         {
--- DAuther/Program.cs
+++ DAuther/Program.cs
@@ -10,41 +10,69 @@
         static string DeviceAuthTokenURL = "https://dauth-lp1.ndas.srv.nintendo.net/v3-59ed5fa1c25bb2aea8c4d73d74b919a94d89ed48d6865b728f63547943b17404/device_auth_token";
         static void Main()
         {
-            string ChallengeData = MakeReq(ChallengeURL, Encoding.UTF8.GetBytes(Challenge));
-            JObject ParseJSON = JObject.Parse(ChallengeData);
-            string ParseChallenge = ParseJSON["challenge"].ToString();
-            byte[] EKS = Convert.FromBase64String(ParseJSON["data"].ToString());
-            if (EKS[8] != DAuth_Src[8])
-            {
-                Console.WriteLine("Error: Keydata changed, please discontinue use of this program until this issue is rectified.");
-                Console.WriteLine(BitConverter.ToString(EKS).Replace("-","") + " was returned, while hardcoded key is " + BitConverter.ToString(DAuth_Src).Replace("-", ""));
-                Environment.Exit(0);
-            }
-            byte[] KEK = GenerateAESKek(MasterKey, AESUseSrc, DAuth_KEK, EKS);
-            string BaseRequest = BuildRequestString(ParseChallenge);
-            string CMAC = GenerateCMACOfRequestString(KEK, BaseRequest);
-            byte[] PostFinal = PostAuthToken(BaseRequest, CMAC);
-            string FinalReq = MakeReq(DeviceAuthTokenURL, PostFinal);
-            JObject ParseFinal = JObject.Parse(FinalReq);
+            string ChallengeData1 = MakeReq(ChallengeURL, Encoding.UTF8.GetBytes(Challenge), 16);
+            JObject ParseJSON1 = JObject.Parse(ChallengeData1);
+            string ParseChallenge1 = ParseJSON1["challenge"].ToString();
+
+            byte[] KeySource = Convert.FromBase64String(ParseJSON1["data"].ToString());
+
+            byte[] KEK = GenerateAESKek(MasterKey, AESUseSrc, DAuth_KEK, KeySource);
+
+            string ChallengeData2 = MakeReq(ChallengeURL, Encoding.UTF8.GetBytes(Challenge), 16);
+            JObject ParseJSON2 = JObject.Parse(ChallengeData1);
+            string ParseChallenge2 = ParseJSON1["challenge"].ToString();
+
+            System.Threading.Thread.Sleep(50);
+
+            string BaseRequest1 = BuildFirstRequestString(ParseChallenge1);
+            string CMAC1 = GenerateCMACOfRequestString(KEK, BaseRequest1);
+            byte[] PostFinal1 = PostAuthToken(BaseRequest1, CMAC1);
+            string FinalReq1 = MakeReq(DeviceAuthTokenURL, PostFinal1, 200);
+            JObject ParseFinal1 = JObject.Parse(FinalReq1);
             try
             {
-                Console.WriteLine("Auth token: " + ParseFinal["device_auth_token"].ToString());
-                Console.WriteLine("Expires at: " + DateTime.Now.AddSeconds(Convert.ToDouble(ParseFinal["expires_in"].ToString())));
-                System.IO.File.WriteAllText("device_auth_token.txt", ParseFinal["device_auth_token"].ToString());
-                Console.WriteLine("Your token was successfully saved to \"device_auth_token.txt\"!");
+                Console.WriteLine("Auth token: " + ParseFinal1["device_auth_token"].ToString());
+                Console.WriteLine("Expires at: " + DateTime.Now.AddSeconds(Convert.ToDouble(ParseFinal1["expires_in"].ToString())));
+                System.IO.File.WriteAllText("device_auth_token_" + Client_ID[0] + ".txt", ParseFinal1["device_auth_token"].ToString());
+                Console.WriteLine("Your first token was successfully saved!");
             }
             catch (Exception)
             {
                 try
                 {
-                    Console.WriteLine("Error " + ParseFinal["errors"][0]["code"].ToString() + ":");
-                    Console.WriteLine(ParseFinal["errors"][0]["message"].ToString());
+                    Console.WriteLine("Error " + ParseFinal1["errors"][0]["code"].ToString() + ":");
+                    Console.WriteLine(ParseFinal1["errors"][0]["message"].ToString());
                 }
                 catch(Exception)
                 {
-                    Console.WriteLine(FinalReq);
+                    Console.WriteLine(FinalReq1);
                 }
-            }          
+            }
+
+            string BaseRequest2 = BuildFirstRequestString(ParseChallenge2);
+            string CMAC2 = GenerateCMACOfRequestString(KEK, BaseRequest2);
+            byte[] PostFinal2 = PostAuthToken(BaseRequest2, CMAC2);
+            string FinalReq2 = MakeReq(DeviceAuthTokenURL, PostFinal2, 200);
+            JObject ParseFinal2 = JObject.Parse(FinalReq2);
+            try
+            {
+                Console.WriteLine("Auth token: " + ParseFinal2["device_auth_token"].ToString());
+                Console.WriteLine("Expires at: " + DateTime.Now.AddSeconds(Convert.ToDouble(ParseFinal2["expires_in"].ToString())));
+                System.IO.File.WriteAllText("device_auth_token_" + Client_ID[1] + ".txt", ParseFinal2["device_auth_token"].ToString());
+                Console.WriteLine("Your second token was successfully saved!");
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    Console.WriteLine("Error " + ParseFinal2["errors"][0]["code"].ToString() + ":");
+                    Console.WriteLine(ParseFinal2["errors"][0]["message"].ToString());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(FinalReq2);
+                }
+            }
         }
     }
 }
